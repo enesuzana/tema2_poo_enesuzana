bb33454621144b7fc33b72b432994b5873dabcb9
Suzi <suzana_ene@yahoo.com>
2019-04-09 01:33:53 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
-
+ifstream fin("test.in");
+ofstream fout("test.out");
 //derived classes need parametrized constructors , destructors
 // constr de init, parametr, de copiere, destructor,
 // >> << = overloaded
@@ -26,7 +27,7 @@ public:
     virtual ~Nod(){
     }
 
-    int getInfo(){
+    virtual int getInfo(){
         return (int)info;
     }
     void setInfo(char val){ info = val; }
@@ -59,12 +60,6 @@ public:
         info = n.info;}
     //destructor
     virtual ~Nod_dublu(){
-        if(ante != NULL){
-            delete ante;
-        }
-        if(next != NULL){
-            delete next;
-        }
     }
     //getteri, setteri
     void setInfo(int inf){ info = inf;}
@@ -74,7 +69,7 @@ public:
     void setAnte(Nod_dublu *nod){ ante = nod; }
     void setNext(Nod_dublu *nod){ next = nod; }
     //= e ok
-    Nod* operator=(const Nod_dublu &nod){
+    Nod_dublu* operator=(const Nod_dublu &nod){
         ante = nod.ante;
         info = nod.info;
         next = nod.next;
@@ -116,16 +111,10 @@ public:
     ListaSimpluInlantuita(const ListaSimpluInlantuita &l);
     ~ListaSimpluInlantuita();
 
-    Nod* getHead(){ return head;}
+    virtual Nod* getHead(){ return head;}
     void setHead(int val){ head->setInfo((int)val);}
     Nod* getTail(){ return tail;}
     void setTail(char val){ tail->setInfo(val);}
-
-    Nod* getNextNod(){ return nod->getNext();}
-    void setNextNod(Nod *next){ nod->setNext(next);}
-
-    int getNodInfo(){ return nod->getInfo();}
-    void setNodInfo(int inf){ nod->setInfo(inf);}
 
     void setCounter(int c){ n = c;}
     int getCounter(){ return n;}
@@ -139,6 +128,7 @@ public:
     virtual ListaSimpluInlantuita operator+(ListaSimpluInlantuita l2);
     friend ostream& operator<<(ostream& out,const ListaSimpluInlantuita &lista);
     friend istream& operator>>(istream& in, ListaSimpluInlantuita &lista);
+
 };
 
 ListaSimpluInlantuita ListaSimpluInlantuita::operator=(ListaSimpluInlantuita l){
@@ -276,32 +266,149 @@ ListaSimpluInlantuita ListaSimpluInlantuita::operator+(ListaSimpluInlantuita l2)
 }
 
 //uses Nod_dublu
-class ListaDubluInlantuita:ListaSimpluInlantuita{
+class ListaDubluInlantuita: public ListaSimpluInlantuita{
     Nod_dublu *head;
     Nod_dublu *tail;
     Nod_dublu *nod;
+    int n;
 public:
-    ListaDubluInlantuita(){}
-    ListaDubluInlantuita(Nod_dublu *head, Nod_dublu *tail, int i):ListaSimpluInlantuita(head, tail, i){}
-    //~ListaDubluInlantuita();
-    void setAnteNod(Nod_dublu *prev){ nod->setAnte(prev); }
-    Nod_dublu* getAnteNod(){ return nod->getAnte(); }
+    ListaDubluInlantuita(){ head = new Nod_dublu(-1, NULL,NULL);
+                            tail = head;
+                            n = 0;}
+    ListaDubluInlantuita(Nod_dublu *h, Nod_dublu *t, int i):ListaSimpluInlantuita(h, t, i){
+        head = h;
+        tail = t;
+        n = i;
+    }
+    ListaDubluInlantuita(const ListaDubluInlantuita &l);
+    ~ListaDubluInlantuita(){}
+
+    Nod_dublu* getHead(){ return head;}
+    void setHead(int val){ head->setInfo((int)val);}
+
+    Nod_dublu* getTail(){ return tail;}
+    void setTail(char val){ tail->setInfo(val);}
+
+    void setCounter(int c){ n = c;}
+    int getCounter(){ return n;}
+
 
     virtual void push(int val, int poz);
-    //virtual void del(int val);
+    virtual void del(int val);
     //operatori:
-    //virtual ListaDubluInlantuita operator=(ListaDubluInlantuita l);
-    //virtual ListaDubluInlantuita operator+(ListaDubluInlantuita l2);
-    //friend ostream& operator<<(ostream& out,const ListaDubluInlantuita &lista);
-    //friend istream& operator>>(istream& in, ListaDubluInlantuita &lista);
+    virtual ListaDubluInlantuita operator=(ListaDubluInlantuita l);
+    virtual ListaDubluInlantuita operator+(ListaDubluInlantuita l2);
+    friend ostream& operator<<(ostream& out,const ListaDubluInlantuita &lista);
+    friend istream& operator>>(istream& in, ListaDubluInlantuita &lista);
 };
+
+ListaDubluInlantuita::ListaDubluInlantuita(const ListaDubluInlantuita &l){
+    head = new Nod_dublu(1, NULL, NULL);
+    tail = head;
+    int k = 0;
+    setCounter(k);
+    Nod *p = l.head;
+    int i;
+    for(i = 0; i < l.n; i++){
+        push(p->getInfo(), i);
+        p = p->getNext();
+    }
+}
+
+ListaDubluInlantuita ListaDubluInlantuita::operator=(ListaDubluInlantuita l){
+    ListaDubluInlantuita l3(l);
+    return l3;
+}
+
+ListaDubluInlantuita ListaDubluInlantuita::operator+(ListaDubluInlantuita l2){
+    ListaDubluInlantuita l3;
+    int k = 0;
+    l3.setCounter(k);
+    Nod_dublu *p = getHead();
+    int i;
+    for(i = 0; i < getCounter(); i++){
+        l3.push(p->getInfo(), i);
+        p = p->getNext();
+    }
+
+    p = l2.getHead();
+
+    for(int k = 0; k < l2.getCounter(); k++){
+        l3.push(p->getInfo(), i);
+        i++;
+        p = p->getNext();
+    }
+
+    return l3;
+}
+
+void ListaDubluInlantuita::del(int val){
+     Nod_dublu *p = head;
+     // daca e primul
+     if( p->getInfo() == val){
+        p->getNext()->setAnte(NULL);
+        head = p->getNext();
+        delete p;
+     }
+     else{//altfel caut pana gasesc valoarea
+        int contor = 0;
+        while( p->getNext()->getInfo() != val ){
+            p = p->getNext();
+            contor++;
+        }
+        if(contor != n-1){
+            Nod_dublu *q = p->getNext();
+            p->setNext(q->getNext());
+            q->getNext()->setAnte(p);
+            delete q;
+        }
+        else{
+            Nod_dublu *q = p->getNext();
+            p->setNext(NULL);
+            tail = p;
+            delete q;
+        }
+     }
+     n--;
+}
+
+ostream& operator<<(ostream& out, ListaDubluInlantuita &lista){
+    Nod_dublu *p = lista.getHead();
+    int c = 0;
+    while(c < lista.getCounter() ){
+        out << p->getInfo() << " ";
+        p = p->getNext();
+        c++;
+    }
+    out << endl;
+    out << "In sens invers: " << endl;
+    p = lista.getTail();
+
+    for(int i = lista.getCounter()-1; i >= 0; i--){
+        out << p->getInfo() << " ";
+        p = p->getAnte();
+    }
+
+
+
+    out << endl;
+    return out;
+}
+
+istream& operator>>(istream& in, ListaDubluInlantuita &lista){
+    int nr;
+    cout<< "Enter val: ";
+    in >> nr;
+    lista.push(nr, lista.getCounter());
+    return in;
+}
 
 void ListaDubluInlantuita::push(int val, int poz){
     if(poz > getCounter() || poz < 0 ){ cout << "Position error";}
     else{
         //decl new nod
         if( head->getInfo() == -1 && poz == 0){cout<<"o creez"<<endl;
-            Nod_dublu *pushed = new Nod_dublu(val, NULL, NULL);
+            Nod_dublu *pushed = new Nod_dublu(val,NULL,NULL);
             head = pushed;
             tail = pushed;
         }
@@ -326,6 +433,7 @@ void ListaDubluInlantuita::push(int val, int poz){
                         p = p->getNext();
                     }
                     pushed->setNext(p->getNext());
+                    p->getNext()->setAnte(pushed);
                     p->setNext(pushed);
                     pushed->setAnte(p);
                 }
@@ -364,7 +472,7 @@ int main(){/*
     //cout << prio3->getNext()->getInfo();
     delete prio3;
     cout << prio3;
-    //priorit nod ok */
+    //priorit nod ok*/
 
     cout<< "exemplu de push in lista, << , >>";
     ListaSimpluInlantuita l1;
@@ -381,15 +489,30 @@ int main(){/*
     l2.push(111,1);
     l2.push(90,2);
     cout<<endl <<"Concatenare:" << endl;
-    //ListaSimpluInlantuita l3= l1+(l2);
+    ListaSimpluInlantuita l3= l1+(l2);
 
     //constructor de copiere:
-    ListaSimpluInlantuita l3(l1);
+    //ListaSimpluInlantuita l3(l1);
     cout << l3;
-    //
-    ListaDubluInlantuita ldb;
-    ldb.push(1,0);
-    ldb.push(10,1);
-    ldb.push(11,2);
+
+
+    ListaDubluInlantuita ld;
+    ld.push(1,0);
+    ld.push(7,1);
+    ld.push(10,2);
+    ld.push(54,0);
+    ld.push(16,1);
+    cout << ld;
+    cin >> ld;
+    cout << ld;
+    ld.del(10);
+    cout<<"dupa stergere 10:"<<ld;
+
+    //exemplu fisier
+    ListaSimpluInlantuita fisierEx;
+    fin >> fisierEx >> fisierEx >> fisierEx;
+    fout << fisierEx;
+
+
     return 0;
 }
